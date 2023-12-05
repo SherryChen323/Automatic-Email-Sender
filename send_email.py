4768ef9e514bae3983498082c0cf1e5ede2dc979
陳柏瑄 <sherrychen323@gmail.com>
2023-12-05 14:09:06 -0800

    Use spintax to send email body

--- send_email.py
+++ send_email.py
@@ -3,6 +3,7 @@ import smtplib
 from email.message import EmailMessage
 from openpyxl import load_workbook
 import time
+import spintax
 
 # Get Receiver data from the excel file
 workbook = load_workbook("Email_Data.xlsx")
@@ -17,6 +18,7 @@ for row in receiver_rows:
         data[title] = cell.value
     receiver_data.append(data)
 
+
 # Get Sender data from the excel file
 sender_sheet = workbook["Sender Info"]
 sender_rows = sender_sheet.rows
@@ -29,14 +31,27 @@ for row in sender_rows:
         data[title] = cell.value
     sender_data.append(data)
 
+
+# Get Email Subject from .txt file
+file_path = 'Email_Subject.txt'
+with open(file_path, 'r') as file:
+    email_subject = file.read()
+
+
+# Get Email Body from .txt file
+file_path = 'Email_Body.txt'
+with open(file_path, 'r') as file:
+    email_body = file.read()
+    
+
 # Send emails
 for data in receiver_data:
     # Define email data
     email_sender = sender_data[0]["Sender Email"]
     email_password = sender_data[0]["Email Password"]
     email_receiver = data["Receiver Email"]
-    subject = data["Email Subject"]
-    body = data["Email body"]
+    subject = email_subject
+    body = spintax.spin(email_body)
 
     em = EmailMessage()
     em['From'] = email_sender
@@ -50,30 +65,9 @@ for data in receiver_data:
     # Log in and send the email
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
         smtp.login(email_sender, email_password)
-        print("Login Success!")
         smtp.sendmail(email_sender, email_receiver, em.as_string())
-        print("Email Sent!")
+        print("One Email Sent!")
         smtp.quit()
-        time.sleep(900)  #5=5 seconds / 900=15 mins
+        time.sleep(900)
 
 print("All Emails Sent!")
-
-
-
-
-
-# ------------------------------------
-# # Attach the image file
-
-# from email.mime.base import MIMEBase
-# from email import encoders
-
-# with open('whaleshark.png', 'rb') as attachment_file:
-#     file_data = attachment_file.read()
-#     file_name = attachment_file.name.split("/")[-1]
-
-# attachment = MIMEBase('application', 'octet-stream')
-# attachment.set_payload(file_data)
-# encoders.encode_base64(attachment)
-# attachment.add_header('Content-Disposition', f'attachment; filename="{file_name}"')
-# em.attach(attachment)
